d9ca3be9653238fa247c83fc6e89868c7c6ce6ad
Ashish Ghimire <117255787+ghimireashish@users.noreply.github.com>
2024-12-15 17:29:53 -0500

    Add files via upload

---
+++ bagh_chal/static/js/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+const Footer = () => {
+    return ( <
+        div className = "footer" >
+        <
+        div className = "footer__copy" >
+        &
+        copy; 2022 Iridescent.All rights reserved. <
+        /div> <
+        /div>
+    );
+}
+
+export default Footer;
